b0be8965b908db351bed6f456c91eb68100b651e
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-02-19 17:13:00 +0100

    Repair property controller

--- FlatAgency/Controllers/PropertyController.cs
+++ FlatAgency/Controllers/PropertyController.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlatAgency.Models.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlatAgency.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PropertyController : ControllerBase
+    [Produces("application/json")]
+    [Route("api/Property")]
+    public class PropertyController : Controller
     {
+        private readonly IPropertyRepository _propertyRepository;
+
+        public PropertyController(IPropertyRepository propertyRepository)
+        {
+            _propertyRepository = propertyRepository;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetProperties()
+        {
+            return new JsonResult(_propertyRepository.GetAll());
+        }
     }
 }

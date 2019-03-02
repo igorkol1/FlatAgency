8e91bd2e9f1c41c100a4b7f52cde26791b88e890
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-03-02 14:55:20 +0100

    Extension of properties component

--- FlatAgency/ClientApp/src/app/components/properties/components/properties.component.ts
+++ FlatAgency/ClientApp/src/app/components/properties/components/properties.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PropertyService } from '../services/property.service';
+import { Router } from '@angular/router';
+import { Property } from 'src/app/models/property';
 
 @Component({
   selector: 'app-properties',
@@ -8,15 +10,45 @@ import { PropertyService } from '../services/property.service';
 })
 export class PropertiesComponent implements OnInit {
 
-  constructor(private propertiesService:PropertyService) { }
+  constructor(
+    private propertiesService: PropertyService,
+    private router: Router
+  ) { }
 
-  test = "Test Properties component"
+  properties: Array<Property> = new Array<Property>();
+  pageTitle: string = "List of flats";
+  tempInfo: string = "Loading..."
 
   ngOnInit() {
+    this.downloadProperties()
+  }
+
+  downloadProperties(): void {
     this.propertiesService.getProperties().subscribe(
-      props => console.log("Properties: "+props),
-      error => console.log("Error: "+error)
+      propertiesFromDb => {
+        if (propertiesFromDb.length == 0) {
+          this.tempInfo = "Records not found"
+        } else {
+          this.properties = propertiesFromDb;
+        }
+      },
+      error => console.log(error)
     )
   }
 
+  getProperty(id:number):void{
+    this.router.navigate(['./properties/property-details',id]);
+  }
+
+  updateProperty(id:number):void{
+    this.router.navigate(['./properties/property-update',id]);
+  }
+
+  deleteProperty(id:number):void{
+    this.propertiesService.deleteProperty(id).subscribe(
+      onSuccess => console.log(onSuccess),
+      onError => console.log(onError)
+    );
+    this.properties.splice(this.properties.findIndex(prop => prop.id==id),1)
+  }
 }
